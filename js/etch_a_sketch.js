2c58ef32851a4655b8d392a6a809f6cd2f97d930
Alex Hardt <hardt.alex@gmail.com>
2015-01-28 16:12:12 -0600

    trying stack overflow grid answer

--- js/etch_a_sketch.js
+++ js/etch_a_sketch.js
@@ -1,18 +1,60 @@
 $(document).ready(function(){
-    $("#cat").click(function(){
-      $(this).slideUp();
-    });
 
-    // begin click event operations
+    var rows = 16,
+        cells = 16,
+        count = 0;
 
-    $("span").click(function(){
+    var i, j,
+        top = 0
+        left = 0
 
-      $("span").text("ahhh, the cat is back!");
+    var boxWidth = 50,
+        boxHeight = 50;
 
-      $("span").slideDown("#cat");
+    var $grid_wrapper = $("#grid_wrapper");
+    var $fragment = $(document.createDocumentFragment());
 
-    }); //ends click event
+    function addBox(opts) {
+      var div = document.createElement('div');
+          div.id = 'item' + opts.count;
+          div.className = "alive";
+          div.style.top = opts.top + "px";
+          div.style.left = opts.left + "px";
 
+          div.innerHTML = "<p>" + opts.count + '</p>';
+          $fragment.append(div);
+    };
+
+    for (j = 0; j < rows; j +=) {
+      top = j * boxHeight;
+
+        for (i=0; i< cells; i += 1) {
+          count += 1;
+
+          addBox({
+            count: count,
+            top: top,
+            left: i * boxWidth
+          });
+        }
+      }
+
+$grid_wrapper.html($fragment);
 
 }); //end doc ready functions
 
+
+
+// $("#cat").click(function(){
+//       $(this).slideUp();
+//     });
+
+//     // begin click event operations
+
+//     $("span").click(function(){
+
+//       $("span").text("ahhh, the cat is back!");
+
+//       $("span").slideDown("#cat");
+
+//     }); //ends click event
