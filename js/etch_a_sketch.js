8347f4d321eef79f003d40a19d5d16b7ce4c6aaa
Alex Hardt <hardt.alex@gmail.com>
2015-01-28 12:27:36 -0600

    got jquerry, need image back

--- js/etch_a_sketch.js
+++ js/etch_a_sketch.js
@@ -2,5 +2,17 @@ $(document).ready(function(){
     $("#cat").click(function(){
       $(this).slideUp();
     });
-});
+
+    // begin click event operations
+
+    $("span").click(function(){
+
+      $("span").text("ahhh, the cat is back!");
+
+      $("span").slideDown("#cat");
+
+    }); //ends click event
+
+
+}); //end doc ready functions
 
