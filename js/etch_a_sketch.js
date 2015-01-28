5d230cb1c6c30bdb52fc233b9e462f97bbd8631a
Alex Hardt <hardt.alex@gmail.com>
2015-01-28 11:25:25 -0600

    css connected, need jquerry, then Ill populate grids

--- js/etch_a_sketch.js
+++ js/etch_a_sketch.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    $("#cat").fadeIn("slow");
+    $("#cat").fadeIn(300);
+
+    // $("#cat").hover(function(){
+    //   (this).effect( "highlight",
+    //       {color:"#669966"}, 3000 );
+
+    });
 });
+
+// .effect( "highlight", {color:"#669966"}, 3000 );
