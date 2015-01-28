a339db127d72c098fbd837449caa2f43a05ebcfd
Alex Hardt <hardt.alex@gmail.com>
2015-01-28 17:46:03 -0600

    technically it works, but im not sure i understand everything

--- js/etch_a_sketch.js
+++ js/etch_a_sketch.js
@@ -1,44 +1,47 @@
 $(document).ready(function(){
 
-    var rows = 16,
-        cells = 16,
-        count = 0;
+var rows = 16,
+    cells = 16,
+    count = 0;
 
-    var i, j,
-        top = 0
-        left = 0
+var i, j,
+    top = 0,
+    left = 0;
 
-    var boxWidth = 50,
-        boxHeight = 50;
+var boxWidth = 50,
+    boxHeight = 50;
 
-    var $grid_wrapper = $("#grid_wrapper");
-    var $fragment = $(document.createDocumentFragment());
+var $grid_wrapper = $('#grid_wrapper');
+var $fragment = $(document.createDocumentFragment());
 
-    function addBox(opts) {
-      var div = document.createElement('div');
-          div.id = 'item' + opts.count;
-          div.className = "alive";
-          div.style.top = opts.top + "px";
-          div.style.left = opts.left + "px";
+function addBox(opts) {
+  var div = document.createElement('div');
+  div.id = opts.id;
+  // div.className = 'alive';
+  div.style.top = opts.top + "px";
+  div.style.left = opts.left + "px";
 
-          div.innerHTML = "<p>" + opts.count + '</p>';
-          $fragment.append(div);
-    };
+  div.innerHTML = '<p>' + opts.count + '</p>';
+  $fragment.append(div);
+};
 
-    for (j = 0; j < rows; j +=) {
-      top = j * boxHeight;
+for (j = 0; j < rows; j += 1) {
+  top = j * boxHeight;
 
-        for (i=0; i< cells; i += 1) {
-          count += 1;
+  for (i = 0; i < cells; i += 1) {
+    count += 1;
 
-          addBox({
-            count: count,
-            top: top,
-            left: i * boxWidth
-          });
-        }
-      }
+    addBox({
+        count: count,
 
+        id: 'item' + i,
+        top: top,
+        left: i * boxWidth
+    });
+  }
+}
+
+// Append all
 $grid_wrapper.html($fragment);
 
 }); //end doc ready functions
---
+++ js/other_test.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  $("#grid_wrapper").append("<div>" + 'hey' + "</div>")
+  // so the above adds a div, now I want to auto increment it and add a lot of them
+
+
+
+});
